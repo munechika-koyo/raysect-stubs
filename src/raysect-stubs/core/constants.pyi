1a586e18a26e86349d4201d2fc7c2e5f7e809255
munechika-koyo <munechika.koyo@gmail.com>
2025-11-28 18:04:25 +0100

    Add docstrings for coordinate system vectors and null transform

--- src/raysect-stubs/core/constants.pyi
+++ src/raysect-stubs/core/constants.pyi
@@ -2,12 +2,20 @@ from .math import AffineMatrix3D, Point3D, Vector3D
 
 # coordinate system vectors
 UP = Vector3D(0, 1, 0)
+"""Up direction Vector3D(0, 1, 0) in Raysect's right-handed coordinate system."""
 DOWN = Vector3D(0, -1, 0)
+"""Down direction Vector3D(0, -1, 0) in Raysect's right-handed coordinate system."""
 LEFT = Vector3D(1, 0, 0)
+"""Left direction Vector3D(1, 0, 0) in Raysect's right-handed coordinate system."""
 RIGHT = Vector3D(-1, 0, 0)
+"""Right direction Vector3D(-1, 0, 0) in Raysect's right-handed coordinate system."""
 FORWARD = Vector3D(0, 0, 1)
+"""Forward direction Vector3D(0, 0, 1) in Raysect's right-handed coordinate system."""
 BACK = Vector3D(0, 0, -1)
+"""Back direction Vector3D(0, 0, -1) in Raysect's right-handed coordinate system."""
 ORIGIN = Point3D()
+"""Origin point Point3D(0, 0, 0) in Raysect's right-handed coordinate system."""
 
 # affine matrix representing no transform
 NULL_TRANSFORM = AffineMatrix3D()
+"""AffineMatrix3D() representing the null transform (no transformation)."""
