b7bad58a4310e5ee40d59d0eb50d06f603ee1d38
munechika-koyo <munechika.koyo@gmail.com>
2025-12-08 10:17:39 +0100

    Add units module

---
+++ src/raysect-stubs/core/math/units.pyi
@@ -0,0 +1,87 @@
+def km(v: float) -> float:
+    """
+    Converts kilometers to meters.
+
+    :param float v: Length in kilometers.
+    :return: Length in meters.
+    """
+
+def cm(v: float) -> float:
+    """
+    Converts centimeters to meters.
+
+    :param float v: Length in centimeters.
+    :return: Length in meters.
+    """
+
+def mm(v: float) -> float:
+    """
+    Converts millimeters to meters.
+
+    :param float v: Length in millimeters.
+    :return: Length in meters.
+    """
+
+def um(v: float) -> float:
+    """
+    Converts micrometers to meters.
+
+    :param float v: Length in micrometers.
+    :return: Length in meters.
+    """
+
+def nm(v: float) -> float:
+    """
+    Converts nanometers to meters.
+
+    :param float v: Length in nanometers.
+    :return: Length in meters.
+    """
+
+def mile(v: float) -> float:
+    """
+    Converts miles to meters.
+
+    :param float v: Length in miles.
+    :return: Length in meters.
+    """
+
+def yard(v: float) -> float:
+    """
+    Converts yards to meters.
+
+    :param float v: Length in yards.
+    :return: Length in meters.
+    """
+
+def foot(v: float) -> float:
+    """
+    Converts feet to meters.
+
+    :param float v: Length in feet.
+    :return: Length in meters.
+    """
+
+def inch(v: float) -> float:
+    """
+    Converts inches to meters.
+
+    :param float v: Length in inches.
+    :return: Length in meters.
+    """
+
+def mil(v: float) -> float:
+    """
+    Converts mils (thousandths of an inch) to meters.
+
+    :param float v: Length in mils.
+    :return: Length in meters.
+    """
+
+def radian(v: float) -> float:
+    """
+    Converts radians to degrees.
+
+    :param float v: Angle in radians.
+    :return: Angle in degrees.
+    """
