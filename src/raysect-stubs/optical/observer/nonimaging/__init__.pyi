0bbdfbb65f534ecf63b1dbf487213a671d91200d
munechika-koyo <munechika.koyo@gmail.com>
2026-07-01 17:10:28 +0200

    Add init file of nonimaging sub-package

---
+++ src/raysect-stubs/optical/observer/nonimaging/__init__.pyi
@@ -0,0 +1,6 @@
+from .fibreoptic import FibreOptic as FibreOptic
+from .mesh_camera import MeshCamera as MeshCamera
+from .mesh_pixel import MeshPixel as MeshPixel
+from .pixel import Pixel as Pixel
+from .sightline import SightLine as SightLine
+from .targeted_pixel import TargetedPixel as TargetedPixel
