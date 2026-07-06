cc7e6de70df8a381298be3da1af1e915db60259d
munechika-koyo <munechika.koyo@gmail.com>
2026-07-06 15:07:54 +0200

    Add init of observer/imaging

---
+++ src/raysect-stubs/optical/observer/imaging/__init__.pyi
@@ -0,0 +1,6 @@
+from .ccd import CCDArray as CCDArray
+from .opencv import OpenCVCamera as OpenCVCamera
+from .orthographic import OrthographicCamera as OrthographicCamera
+from .pinhole import PinholeCamera as PinholeCamera
+from .targeted_ccd import TargetedCCDArray as TargetedCCDArray
+from .vector import VectorCamera as VectorCamera
