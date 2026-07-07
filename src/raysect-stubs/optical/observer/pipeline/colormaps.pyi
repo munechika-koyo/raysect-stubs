7ce8916904ff63f9497407a2d105b8587f8e33e9
munechika-koyo <munechika.koyo@gmail.com>
2026-07-07 10:31:41 +0200

    Add colormaps module

---
+++ src/raysect-stubs/optical/observer/pipeline/colormaps.pyi
@@ -0,0 +1,8 @@
+from matplotlib.colors import ListedColormap
+
+__all__ = ["magma", "inferno", "plasma", "viridis"]
+
+magma: ListedColormap
+inferno: ListedColormap
+plasma: ListedColormap
+viridis: ListedColormap
